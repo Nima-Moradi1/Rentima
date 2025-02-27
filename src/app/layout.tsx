ff005e630a1393447c8968fa91d3dc6eca169bf6
nima moradi <mnima8100@gmail.com>
2025-02-27 10:38:02 +0330

    changed font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,16 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Outfit } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "../components/Header";
 import MaxWidthWrapper from "../components/ui/MaxWidthWrapper";
 
 
-const geistSans = Geist({
+const Inter = Outfit({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Rentima ",
@@ -29,7 +25,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <body className={`${Inter.variable} antialiased`}>
           <Header />
           <MaxWidthWrapper>
           {children}
